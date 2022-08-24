60ddd7382e9a4ca3cc34cd71f201867dc08baf7e
Jere <jerebizai@gmail.com>
2022-08-24 05:47:55 -0300

    12.0

---
+++ Proyecto final/Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] PlayerInput playerInput;
+
+    //variables readonly que informan al PlayerController el status de nuestros input
+    public Vector2 Move {get; private set;}
+    public Vector2 Look {get; private set;}
+    public bool Run {get; private set;}
+
+    //ref o hook de cada tipo, para acceder al input map y a cada input action individual
+    InputActionMap currentMap;
+    InputAction moveAction;
+    InputAction lookAction;
+    InputAction runAction;
+
+    void Awake()
+    {  
+        HideCursor();
+
+        //aca obtenemos nuestro InputActionMap y actions particulares, seteando refs, para poder manipularlos localmente
+        currentMap = playerInput.currentActionMap;
+        moveAction = currentMap.FindAction("Move");
+        lookAction = currentMap.FindAction("Look");
+        runAction = currentMap.FindAction("Run");
+        
+        //aca suscribimos cada funcion callback a cada input action
+        moveAction.performed += onMove;
+        lookAction.performed += onLook;
+        runAction.performed += onRun;
+
+        //performed es cuando el keybinding empieza y canceled cuando termina
+        moveAction.canceled += onMove;
+        lookAction.canceled += onLook;
+        runAction.canceled += onRun;
+    }
+
+    void HideCursor()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    //aca creamos cada funcion callback a la que asociamos cada input action
+    void onMove(InputAction.CallbackContext context) //cada una tiene sus propios parametros callback [] profundizar
+    {
+        Move = context.ReadValue<Vector2>();
+    }
+        void onLook(InputAction.CallbackContext context)
+    {
+        Look = context.ReadValue<Vector2>();
+    }
+        void onRun(InputAction.CallbackContext context) 
+    {
+        Run = context.ReadValueAsButton();
+    }
+
+    //esto es para "organizar" el proyecto, no pude comprenderlo mucho
+    void OnDisable() {
+        currentMap.Disable();
+    }
+}
---
+++ Proyecto final/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float animBlendSpeed = 9f;
+    Rigidbody playerRigidbody;
+    InputManager inputManager;
+
+    //para acceder a nuestra animation stuff
+    Animator animator;
+    bool hasAnimator;
+
+    //las propiedades que seteamos en nuestro animador, necesitamos sus hashs
+    int xVelHash;
+    int yVelHash;
+
+    //las velocidades que definimos en el blend tree, en AnimPlayerController
+    const float walkSpeed = 3f;
+    const float runSpeed = 5f;
+
+    //la representacion 2d de nuestra velocidad
+    Vector2 currentVelocity;
+
+    //camera thingz [] dps ver si se puede lograr lo mismo usando cinemachine
+    [SerializeField] Transform cameraTarget;
+    [SerializeField] Transform camera;
+    [SerializeField] float upperLimit = -40f;
+    [SerializeField] float bottomLimit = 70f;
+    [SerializeField] float mouseSensitivity = 20f;
+    float xRotation;
+
+    void Start()
+    {
+        //inicializando los componentes y variables...
+        hasAnimator = TryGetComponent<Animator>(out animator);
+        playerRigidbody = GetComponent<Rigidbody>();
+        inputManager = GetComponent<InputManager>();
+
+        xVelHash = Animator.StringToHash("xVelocity");
+        yVelHash = Animator.StringToHash("yVelocity");
+
+    }
+
+    void FixedUpdate() //ya que estamos usando rigidbody
+    {
+        Move();
+    }
+
+    void LateUpdate()
+    {
+        CameraMovement();
+    }
+
+    void Move()
+    {
+        if (!hasAnimator) return; //comprobamos que el player tenga un Animator, sino la funcion Move no se ejecuta
+
+        //definimos target speed segun cada caso
+        float targetSpeed = inputManager.Run ? runSpeed : walkSpeed;
+        if (inputManager.Move == Vector2.zero) targetSpeed = 0.1f;
+
+        //aca definimos nuestra currentVelocity en tanto la targetSpeed
+        currentVelocity.x = Mathf.Lerp(currentVelocity.x, inputManager.Move.x * targetSpeed, animBlendSpeed * Time.fixedDeltaTime);
+        currentVelocity.y = Mathf.Lerp(currentVelocity.y, inputManager.Move.y * targetSpeed, animBlendSpeed * Time.fixedDeltaTime);
+
+        //esto es para que el rigidbody no acelere infinitamente (?)
+        var xVelDifference = currentVelocity.x - playerRigidbody.velocity.x;
+        var zVelDifference = currentVelocity.y - playerRigidbody.velocity.z;
+
+        //ahora definimos la velocidad del rigidbody en tanto nuestra currentVelocity
+        playerRigidbody.AddForce(transform.TransformVector(new Vector3(xVelDifference, 0, zVelDifference)), ForceMode.VelocityChange); //siendo que calculamos en modo local, queremos transferir nuestra velocidad local a global
+
+        //pasamos al Animator nuestra currentVelocity, para animar adecuadamente
+        animator.SetFloat(xVelHash, currentVelocity.x);
+        animator.SetFloat(yVelHash, currentVelocity.y);
+    }
+
+    void CameraMovement()
+    {
+        if (!hasAnimator) return;
+
+        //aca captamos el input del inputmanager
+        var mouseX = inputManager.Look.x;
+        var mouseY = inputManager.Look.y;
+
+        camera.position = cameraTarget.position;
+
+        xRotation -= mouseY * mouseSensitivity * Time.deltaTime;
+        xRotation = Mathf.Clamp(xRotation, upperLimit, bottomLimit);
+
+        camera.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.Rotate(Vector3.up, mouseX * mouseSensitivity * Time.deltaTime);
+    }
+}
